9e499025ce9ee7bdbe0522d5386029a7b36f59f6
Howard Symes Thompson <howard.symes.thompson@gmail.com>
2023-11-15 16:46:26 +0000

    yelp mvp challenge

--- Gemfile
+++ Gemfile
@@ -5,6 +5,12 @@ ruby "3.1.2"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.2"
 
+gem 'psych', '~>3.0'
+
+gem "simple_form"
+
+gem 'faker'
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+
+puts "Creating restaurants..."
+50.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  phone_number = Faker::PhoneNumber.phone_number
+  category = ['Chinese', 'Italian', 'Japanese', 'French', 'Belgian'].sample
+  name = Restaurant.create!(name:, address:, phone_number:, category:)
+end
+
+puts "Finished!"
